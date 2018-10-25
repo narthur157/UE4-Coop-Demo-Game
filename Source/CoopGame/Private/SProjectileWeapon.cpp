6ef1106cf72aca06f32b7a7a8bbf7c28b5ea5319
Joe <joefinleybusiness@gmail.com>
2018-10-25 19:10:16 -0400

    Projectile weapon replicated

--- Source/CoopGame/Private/SProjectileWeapon.cpp
+++ Source/CoopGame/Private/SProjectileWeapon.cpp
@@ -8,6 +8,12 @@
 
 void ASProjectileWeapon::Fire()
 {
+    if (Role < ROLE_Authority)
+    {
+        ServerFire();
+        return;
+    }
+
     if (ProjectileClass && GetOwner())
     {
         FVector ProjectileSpawnLocation = MeshComp->GetSocketLocation(MuzzleSocketName);
@@ -16,9 +22,23 @@ void ASProjectileWeapon::Fire()
         GetOwner()->GetActorEyesViewPoint(OutViewPointLocation, ProjectileSpawnRotation);
 
         FActorSpawnParameters SpawnParams;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
         SpawnParams.Instigator = Cast<APawn>(GetOwner());
         ASProjectile* NewProjectile = GetWorld()->SpawnActor<ASProjectile>(ProjectileClass, ProjectileSpawnLocation, ProjectileSpawnRotation, SpawnParams);
+
+        if (Role == ROLE_Authority)
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Server: SpawnLocaton: %s"), *ProjectileSpawnLocation.ToString());
+
+        }
+        else
+        {
+            UE_LOG(LogTemp, Warning, TEXT("SpawnLocaton: %s"), *ProjectileSpawnLocation.ToString());
+
+        }
+
         NewProjectile->Initialize(ProjectileWeaponConfigData);
         NewProjectile->Launch();    
+
     }
 }
