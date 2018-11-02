4c7c2b2e64e9b682815f54e750117be5267e56c0
Joe <joefinleybusiness@gmail.com>
2018-11-01 22:35:39 -0400

    Naive in game menu implementation, issues with locking input

---
+++ Source/CoopGame/Private/SPlayerController.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SPlayerController.h"
+#include "Engine.h"
+#include "Blueprint/UserWidget.h"
+
+
+
+void ASPlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    ensure(InputComponent);
+
+    InputComponent->BindAction("ToggleMenu", IE_Pressed, this, &ASPlayerController::ToggleMenu);
+
+}
+
+void ASPlayerController::ToggleMenu()
+{
+    if (!MenuRef)
+    {
+        MenuRef = CreateWidget<UUserWidget>(this, MenuWidgetClass);
+        if (MenuRef)
+        {
+            MenuRef->AddToViewport();
+            MenuRef->SetVisibility(ESlateVisibility::Visible);
+            GEngine->GameViewport->Viewport->LockMouseToViewport(false);
+            bShowMouseCursor = true;
+            
+        }
+    }
+    else
+    {
+        if (MenuRef->IsVisible())
+        {
+            MenuRef->SetVisibility(ESlateVisibility::Hidden);
+            GEngine->GameViewport->Viewport->LockMouseToViewport(true);
+            bShowMouseCursor = false;
+
+        }
+        else
+        {
+            MenuRef->SetVisibility(ESlateVisibility::Visible);
+            GEngine->GameViewport->Viewport->LockMouseToViewport(false);
+            bShowMouseCursor = true;
+            
+
+
+        }
+    }
+}
+
