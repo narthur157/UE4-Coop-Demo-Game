f3aef4a0d3fb272a04ffad2c0f4482d98f2ddd55
Joe <joefinleybusiness@gmail.com>
2018-11-03 04:37:30 -0400

    Gamemode subclassing

---
+++ Source/CoopGame/Private/SHordeGameMode.cpp
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SHordeGameMode.h"
+#include "SGameState.h"
+#include "CoopGame.h"
+#include "SHealthComponent.h"
+#include "SPlayerState.h"
+
+
+
+ASHordeGameMode::ASHordeGameMode()
+{
+    GameStateClass = ASGameState::StaticClass();
+    PlayerStateClass = ASPlayerState::StaticClass();
+    ActorKilled.AddDynamic(this, &ASHordeGameMode::OnActorKilled);
+}
+
+void ASHordeGameMode::StartPlay()
+{
+    Super::StartPlay();
+
+    PrepareForNextWave();
+}
+
+
+void ASHordeGameMode::StartWave()
+{
+    SetWaveState(EWaveState::WaitingToComplete);
+    CurrentWaveCount++;
+    NumberBotsToSpawn = 2 * CurrentWaveCount;
+    GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASHordeGameMode::SpawnBotTimerElapsed, 1.0f, true, 0.0f);
+}
+
+void ASHordeGameMode::SpawnBotTimerElapsed()
+{
+    SpawnNewBot();
+    NumberBotsToSpawn--;
+    if (NumberBotsToSpawn <= 0)
+    {
+        EndWave();
+    }
+}
+
+void ASHordeGameMode::EndWave()
+{
+    GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+    SetWaveState(EWaveState::WaitingToComplete);
+}
+
+void ASHordeGameMode::PrepareForNextWave()
+{
+    RestartDeadPlayers();
+    // Game is finished, players win on waves
+    if (CurrentWaveCount == NumberWaves)
+    {
+        GameOver(true);
+        return;
+    }
+    // Work on spawning the next wave
+    GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASHordeGameMode::StartWave, TimeBetweenWaves, false);
+    SetWaveState(EWaveState::WaitingToStart);
+
+}
+
+// Iterate over all of the pawns, if we find a bot and its still alive then carry on
+// This should really be generalized more into a team format somehow? broadcast win/lose conditions based on teamID
+void ASHordeGameMode::CheckWaveState()
+{
+    if (NumberBotsToSpawn > 0 || GetWorldTimerManager().IsTimerActive(TimerHandle_NextWaveStart))
+    {
+        return;
+    }
+
+    bool bIsAnyBotAlive = false;
+    for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; ++It)
+    {
+        // If we are player controlled
+        APawn* TestPawn = It->Get();
+        if (!TestPawn || TestPawn->IsPlayerControlled())
+        {
+            continue;
+        }
+        USHealthComponent* HealthComp = TestPawn->FindComponentByClass<USHealthComponent>();
+        if (HealthComp && HealthComp->GetHealth() > 0)
+        {
+            // Bot is still alive
+            bIsAnyBotAlive = true;
+            break;
+        }
+    }
+    if (!bIsAnyBotAlive)
+    {
+        TRACE("Bots have all died");
+        SetWaveState(EWaveState::WaveComplete);
+        PrepareForNextWave();
+    }
+}
+
+// Changes the state of the wave spawner
+void ASHordeGameMode::SetWaveState(EWaveState State)
+{
+    ASGameState* GS = GetGameState<ASGameState>();
+    if (ensureAlways(GS))
+    {
+        GS->SetWaveState(State);
+    }
+}
+
+
+// Called when a player has died, used in order to avoid using tick to check player/wave state
+void ASHordeGameMode::OnActorKilled_Implementation(AActor* KilledActor, AActor* KillerActor, AController* KillerController)
+{
+    // TODO: We only really need to check one or the other based on whether or not the killed actor is a player
+    CheckPlayerState();
+    CheckWaveState();
+}
--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -6,7 +6,6 @@
 #include "GameFramework/GameModeBase.h"
 #include "SGameMode.generated.h"
 
-enum class EWaveState : uint8;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnActorKilled, AActor*, KilledActor, AActor*, KillerActor, AController*, KillerController);
 
@@ -28,38 +27,7 @@ public:
 	
 protected:
 
-    // Wave spawning
-    UPROPERTY(EditDefaultsOnly, Category = "WaveMode")
-    int32 NumberWaves = 1;
-
-    // Preparation time between waves
-    UPROPERTY(EditDefaultsOnly, Category = "WaveMode")
-    float TimeBetweenWaves = 0.0f;
-
-    int32 CurrentWaveCount = 0;
-
-    // Integer used to control how many bots we are spawning in the current wave
-    int32 NumberBotsToSpawn = 0;
-
-    FTimerHandle TimerHandle_BotSpawner;
-    FTimerHandle TimerHandle_NextWaveStart;
-
-    // Spawns a bot using the blueprint override
-    UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
-    void SpawnNewBot();
-
-    void SpawnBotTimerElapsed();
-
-    void StartWave();
-
-    void EndWave();
-
-    void PrepareForNextWave();
-
-    void SetWaveState(EWaveState State);
-
-    void CheckWaveState();
-
+    
     // Gamestate handlers 
     void CheckPlayerState();
 
@@ -72,5 +40,6 @@ protected:
     
     UFUNCTION(BlueprintNativeEvent, Category = "GameMode")
     void OnActorKilled(AActor* KilledActor, AActor* KillerActor, AController* KillerController);
+    virtual void OnActorKilled_Implementation(AActor* KilledActor, AActor* KillerActor, AController* KillerController);
 
 };
