40c6f8e09ce0863b324941b8fbb61785b693025f
Joe <joefinleybusiness@gmail.com>
2018-11-06 13:34:14 -0500

    Destroyable objective base class

---
+++ Source/CoopGame/Private/DefenseObjective.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DefenseObjective.h"
+#include "Net/UnrealNetwork.h"
+#include "SHealthComponent.h"
+
+// Sets default values
+ADefenseObjective::ADefenseObjective()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+    SetReplicates(true);
+    HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComponent"));
+}
+
+void ADefenseObjective::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(ADefenseObjective, bObjectiveDestroyed);
+
+}
+
+// Called when the game starts or when spawned
+void ADefenseObjective::BeginPlay()
+{
+	Super::BeginPlay();
+    HealthComp->OnHealthChanged.AddDynamic(this, &ADefenseObjective::OnHealthComponentTakeDamage);
+
+}
+
+uint8 ADefenseObjective::GetTeamID()
+{
+    return HealthComp->TeamNum;
+}
+
+void ADefenseObjective::OnRep_ObjectiveDestroyed()
+{
+    OnObjectiveDestroyed();
+}
