9bf977106a75e9baa9dd9e8617cf50572ba29d22
Joe <joefinleybusiness@gmail.com>
2018-11-09 14:41:26 -0500

    Subclass SHordeGameState from SGameState

--- Source/CoopGame/Private/Game/SGameState.cpp
+++ Source/CoopGame/Private/Game/SGameState.cpp
@@ -6,30 +6,6 @@
 #include "Net/UnrealNetwork.h"
 
 
-void ASGameState::OnRep_WaveStateChanged(EWaveState OldState)
-{
-    WaveStateChanged(WaveState, OldState);
-    OnWaveStateChanged.Broadcast(OldState, WaveState);
-}
-
-void ASGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
-    DOREPLIFETIME(ASGameState, WaveState);
-    DOREPLIFETIME(ASGameState, NumEnemiesAlive);
-    DOREPLIFETIME(ASGameState, NextWaveStartTime);
-}
-
-void ASGameState::SetWaveState(EWaveState NewState)
-{
-    if (Role == ROLE_Authority)
-    {
-        EWaveState OldState = WaveState;
-        WaveState = NewState;
-        OnRep_WaveStateChanged(OldState);
-    }
-}
 
 void ASGameState::MulticastGameOver_Implementation(bool bWasSuccessful)
 {
@@ -40,11 +16,5 @@ void ASGameState::MulticastGameOver_Implementation(bool bWasSuccessful)
 void ASGameState::MulticastActorKilled_Implementation(AActor* KilledActor, AActor* KillerActor, AActor* DamageCauser)
 {
     OnActorKilledGameState.Broadcast(KilledActor, KillerActor, DamageCauser);
-}
-
-void ASGameState::OnRep_NumEnemiesChanged()
-{
-    TRACE("NumberEnemiesChanged");
-    OnNumEnemiesChanged.Broadcast(NumEnemiesAlive);
 }
 
--- Source/CoopGame/Public/Game/SGameState.h
+++ Source/CoopGame/Public/Game/SGameState.h
@@ -6,28 +6,12 @@
 #include "GameFramework/GameStateBase.h"
 #include "SGameState.generated.h"
 
-UENUM(BlueprintType)
-enum class EWaveState : uint8
-{
-    None,
-    WaitingToStart,
-    WaitingToComplete,
-    WaveComplete,
-    GameOver
-
-};
-
 /** Broadcasts when an actor has been killed */
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FGameStateActorKilled, AActor*, KilledActor, AActor*, KillerActor, AActor*, DamageCauser);
 
 /** Broadcasts when the game has ended */
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FGameStateGameOver, bool, bWasSuccessful);
 
-/** Broadcasts when the game has ended */
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FGameStateNumEnemiesChanged, int32, NumEnemiesAlive);
-
-/** Broadcasts when the wave state has changed */
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FGameStateWaveChanged, EWaveState, OldState,  EWaveState, NewWaveState);
 
 /**
  * 
@@ -38,16 +22,7 @@ class COOPGAME_API ASGameState : public AGameStateBase
 	GENERATED_BODY()
 	
 public:
-
-    UPROPERTY(BlueprintReadOnly, Replicated, Category = "WaveState")
-    float NextWaveStartTime = 0.0f;
-
-    UPROPERTY(BlueprintReadWrite, ReplicatedUsing = OnRep_NumEnemiesChanged, Category = "WaveState")
-    int32 NumEnemiesAlive = 0;
-
-    UFUNCTION(BlueprintCallable, Category = "WaveState")
-    void SetWaveState(EWaveState NewState);
-
+  
     UFUNCTION(NetMulticast, Reliable)
     void MulticastGameOver(bool bWasSuccessful);
 
@@ -60,24 +35,4 @@ public:
     UPROPERTY(BlueprintAssignable, Category = "GameEvent")
     FGameStateGameOver OnGameOver;
 
-    UPROPERTY(BlueprintAssignable, Category = "WaveState")
-    FGameStateWaveChanged OnWaveStateChanged;
-
-    UPROPERTY(BlueprintAssignable, Category = "GameObjective")
-    FGameStateNumEnemiesChanged OnNumEnemiesChanged;
-
-    UFUNCTION(BlueprintCallable, Category = "WaveState")
-    void OnRep_NumEnemiesChanged();
-
-protected:
-    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_WaveStateChanged, Category = "WaveState")
-    EWaveState WaveState = EWaveState::None;
-
-    UFUNCTION()
-    void OnRep_WaveStateChanged(EWaveState OldState);
-	
-    UFUNCTION(BlueprintImplementableEvent, Category = "WaveState")
-    void WaveStateChanged(EWaveState NewState, EWaveState OldState);
-
-   
 };
