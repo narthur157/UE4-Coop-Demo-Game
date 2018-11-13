50ee62d1ac16004f3e1a3c611884ae42e6c08a9c
Joe <joefinleybusiness@gmail.com>
2018-11-12 22:50:21 -0500

    SHUD naive implementation, minor refactoring of gamemodes/gamestates

--- Source/CoopGame/Private/Game/SGameMode.cpp
+++ Source/CoopGame/Private/Game/SGameMode.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SGameMode.h"
-#include "CoopGame.h"
-#include "SGameState.h"
 #include "SHealthComponent.h"
-#include "SPlayerController.h"
 #include "SPlayerState.h"
+#include "SGameState.h"
+#include "CoopGame.h"
+
 
 ASGameMode::ASGameMode()
 {
@@ -67,9 +67,14 @@ void ASGameMode::RestartDeadPlayers()
     }
 }
 
+void ASGameMode::InitializeHUDForPlayer_Implementation(APlayerController * NewPlayer)
+{
+    NewPlayer->ClientSetHUD(HUDClass);
+}
+
 // Called when a player has died, used in order to avoid using tick to check player/wave state
 void ASGameMode::OnActorKilled_Implementation(AActor* KilledActor, AActor* KillerActor, AActor* DamageCauser)
 {
-  
+    
 }
 
