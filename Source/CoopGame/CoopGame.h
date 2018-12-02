77d189ee1fafd347fb95f4420bf0a7bad4b7202a
Nicholas Arthur <narthur157@gmail.com>
2018-12-02 13:01:27 -0500

    Add engine.h to coopgame.h so that GEngine is defined for trace macro

--- Source/CoopGame/CoopGame.h
+++ Source/CoopGame/CoopGame.h
@@ -1,8 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine.h"
 
 
 #define MAX_NAME_LENGTH             15
