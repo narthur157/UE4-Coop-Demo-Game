61b2b37ed4a797bf11ad4f0c895c5de78c852b60
Nicholas Arthur <narthur157@gmail.com>
2018-12-06 14:18:16 -0500

    Replace join by IP with steam sessions, update main menu with new sessions UI

--- Source/CoopGame.Target.cs
+++ Source/CoopGame.Target.cs
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
@@ -8,7 +6,7 @@ public class CoopGameTarget : TargetRules
 	public CoopGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-
+        bUsesSteam = true;
 		ExtraModuleNames.AddRange( new string[] { "CoopGame" } );
 	}
 }
